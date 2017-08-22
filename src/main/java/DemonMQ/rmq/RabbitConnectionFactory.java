e6b3997ce0ec244f22728691d8b5a2add487f09c
DemonWang <houziwty@126.com>
2017-08-22 18:04:14 +0800

    更新

--- src/main/java/DemonMQ/rmq/RabbitConnectionFactory.java
+++ src/main/java/DemonMQ/rmq/RabbitConnectionFactory.java
@@ -1,5 +1,7 @@
 package demonmq.rmq;
 
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,22 +30,74 @@ public class RabbitConnectionFactory {
     public static int connectionCacheSize = defaultConnectionCacheSize;
     public static int channelCacheSize = defaultChannelCacheSize;
 
-    private  static Lock connectionLock=new ReentrantLock();
+    private static Lock connectionLock = new ReentrantLock();
 
-    private static Lock channelLock=new ReentrantLock();
+    private static Lock channelLock = new ReentrantLock();
 
     //连接工厂缓存
-    private static Map<RabbitConnectionBean,ConnectionFactory>factoryMap=new HashMap<>();
+    private static Map<RabbitConnectionBean, ConnectionFactory> factoryMap = new HashMap<>();
 
-    private static  Map<RabbitConnectionBean, List<RabbitConnection>>connectionMap=new HashMap<>();
+    private static Map<RabbitConnectionBean, List<RabbitConnection>> connectionMap = new HashMap<>();
 
-    private static AtomicInteger channelId=new AtomicInteger(0);
+    /**
+     * 通道缓存
+     */
+    static final Map<Integer, RabbitChannel> channelCache = new HashMap<>();
 
-    public static  class RabbitConnection{
 
+    private static AtomicInteger channelId = new AtomicInteger(0);
+
+
+    private static ConnectionFactory getConnectionFactory(RabbitConnectionBean bean){
+ConnectionFactory connectionFactory=factoryMap.get(bean);
+        return connectionFactory;
+    }
+
+    public static class RabbitConnection {
+        private Connection connection;
+        private List<RabbitChannel> channelList;
+
+        public Connection getConnection() {
+            return connection;
+        }
+
+        public void setConnection(Connection connection) {
+            this.connection = connection;
+        }
+
+        public List<RabbitChannel> getChannelList() {
+            return channelList;
+        }
+
+        public void setChannelList(List<RabbitChannel> channelList) {
+            this.channelList = channelList;
+        }
     }
 
     public static class RabbitChannel {
+        private int channelId;
 
+        private Channel channel;
+        public RabbitChannel() {
+        }
+        public RabbitChannel(int channelId, Channel channel) {
+            this.channelId = channelId;
+            this.channel = channel;
+        }
+        public Channel getChannel() {
+            return channel;
+        }
+
+        public void setChannel(Channel channel) {
+            this.channel = channel;
+        }
+
+        public int getChannelId() {
+            return channelId;
+        }
+
+        public void setChannelId(int channelId) {
+            this.channelId = channelId;
+        }
     }
 }
